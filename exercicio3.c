3136a154e7efe5ebdf28f25b0be2d13e6ecd4a2a
octaviopalmieri00-ops <octaviopalmieri00@gmail.com>
2026-08-19 14:45:10 -0300

    Update exercicio3.c

--- exercicio3.c
+++ exercicio3.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int main()
+{
+    float nota1;
+    float nota2;
+    float nota3;
+    float media;
+
+    printf("Digite a primeira nota: ");
+    scanf("%f", &nota1);
+
+    printf("Digite a segunda nota: ");
+    scanf("%f", &nota2);
+
+    printf("Digite a terceira nota: ");
+    scanf("%f", &nota3);
+
+    media = (nota1 + nota2 + nota3) / 3;
+
+    printf("A media e %.2f", media);
+
+    return 0;
+}
